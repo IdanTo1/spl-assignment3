f8a42a075783b5415320f0f188f8bff82bf7bbf9
Hroy02 <hersh.roy@gmail.com>
2020-01-07 17:44:42 +0200

    Add libraryProtocol

--- src/main/java/bgu/spl/net/impl/stomp/LibraryProtocol.java
+++ src/main/java/bgu/spl/net/impl/stomp/LibraryProtocol.java
@@ -28,23 +28,41 @@ public class LibraryProtocol implements StompMessagingProtocol<Frame> {
     public void process(Frame msg) {
         switch(msg.getCommand()) {
             case CONNECT:
-                if(!msg.getHeader("accept-version").equals("1.2")){
-                    _shouldTerminate = true;
-                    _connections.send(_connectionId, new Frame("ERROR", "Wrong Version"));
-                }
-                else if((_client = clientsByLogin.get(msg.getHeader("login"))) == null) {
-                    _client = new Client(msg.getHeader("host"), msg.getHeader("login"), msg.getHeader("passcode"));
-                   clientsByLogin.put(_client.login, _client);
-                   Frame frame = new Frame("CONNECTED");
-                   frame.addHeader("version", "1.2");
-                   _connections.send(_connectionId, frame);
-                }
-                else if(_client.passcode != msg.getHeader("passcode")) {
-                    _shouldTerminate = true;
-                    _connections.send(_connectionId, new Frame("ERROR", "Wrong password"));
-                }
+                handleConnect(msg);
+                break;
+            case SEND:
+                handleSend(msg);
                 break;
         }
+    }
+
+    private void handleSend(Frame msg) {
+
+    }
+
+    private void handleConnect(Frame msg) {
+        if(!msg.getHeader("accept-version").equals("1.2")){
+            _shouldTerminate = true;
+            _connections.send(_connectionId, new Frame("ERROR", "Wrong Version"));
+        }
+        else if((_client = clientsByLogin.get(msg.getHeader("login"))) == null) {
+            _client = new Client(msg.getHeader("host"), msg.getHeader("login"), msg.getHeader("passcode"));
+            clientsByLogin.put(_client.login, _client);
+            sendConnectedFrame();
+        }
+        else if(_client.passcode.equals(msg.getHeader("passcode"))) {
+            sendConnectedFrame();
+        }
+        else {
+            _shouldTerminate = true;
+            _connections.send(_connectionId, new Frame("ERROR", "Wrong password"));
+        }
+    }
+
+    private void sendConnectedFrame() {
+        Frame frame = new Frame("CONNECTED");
+        frame.addHeader("version", "1.2");
+        _connections.send(_connectionId, frame);
     }
 
     @Override
