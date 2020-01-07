2cc5315706ba12472cd5511da7be9267837ed6b3
Hroy02 <hersh.roy@gmail.com>
2020-01-07 18:56:08 +0200

    Fix ConnectionsImpl

--- src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -3,14 +3,15 @@ package bgu.spl.net.impl.stomp;
 import bgu.spl.net.srv.ConnectionHandler;
 import bgu.spl.net.srv.Connections;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ConnectionsImpl<T> implements Connections<T> {
+public class ConnectionsImpl implements Connections<Frame> {
 
-    private ConcurrentHashMap<Integer, ConnectionHandler<T>> _connections;
-    private ConcurrentHashMap<String, List<Integer>> _channels;
+    private ConcurrentHashMap<Integer, ConnectionHandler<Frame>> _connections;
+    private ConcurrentHashMap<String, List<AbstractMap.SimpleEntry<ConnectionHandler<Frame>, Integer>>> _channels;
 
     public ConnectionsImpl() {
         _connections = new ConcurrentHashMap<>();
@@ -18,7 +19,7 @@ public class ConnectionsImpl<T> implements Connections<T> {
     }
 
     @Override
-    public boolean send(int connectionId, T msg) {
+    public boolean send(int connectionId, Frame msg) {
         try {
             _connections.get(connectionId).send(msg);
         }
@@ -28,31 +29,37 @@ public class ConnectionsImpl<T> implements Connections<T> {
         return true;
     }
 
+    private Frame addSubscriptionId(Frame msg, Integer id) {
+        Frame f = msg.clone();
+        f.addHeader("subscription", id.toString());
+        return f;
+    }
+
     @Override
-    public void send(String channel, T msg) {
-        for(Integer i : _channels.get(channel)) {
-            _connections.get(i).send(msg);
+    public void send(String channel, Frame msg) {
+        for(AbstractMap.SimpleEntry<ConnectionHandler<Frame>, Integer> sub : _channels.get(channel)) {
+            sub.getKey().send(addSubscriptionId(msg, sub.getValue())); // Send the message with the subscription header
         }
     }
 
     @Override
     public void disconnect(int connectionId) {
-        for(List<Integer> topic : _channels.values()) {
+        for(List<AbstractMap.SimpleEntry<ConnectionHandler<Frame>, Integer>> topic : _channels.values()) {
             topic.remove(connectionId);
         }
         _connections.remove(connectionId);
     }
 
     @Override
-    public void subscribe(String channel, int connectionId) {
+    public void subscribe(String channel, int connectionId, int subscriptionId) {
         synchronized (this) {
             _channels.computeIfAbsent(channel, k -> new ArrayList<>());
         }
-        _channels.get(channel).add(connectionId);
+        _channels.get(channel).add(new AbstractMap.SimpleEntry<>(_connections.get(connectionId), subscriptionId));
     }
 
     @Override
-    public void connect(ConnectionHandler<T> connectionHandler, int connectionId) {
+    public void connect(ConnectionHandler<Frame> connectionHandler, int connectionId) {
         _connections.put(connectionId, connectionHandler);
     }
 }
